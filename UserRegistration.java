9dbf0040818473e2e12dc22dc65e8eafca26004f
RAHUL LABADE <rahullabade05@gmail.com>
2021-10-28 05:54:22 +0530

    Added: UC1_UserRegistration

---
+++ UserRegistration.java
@@ -0,0 +1,10 @@
+package UserRegistration;
+
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+	public boolean firstName() {
+		boolean matchesString = Pattern.matches("^[A-Z]{1}[a-zA-Z]{3,10}$", "Rahul");
+		return matchesString;
+	}
+}
---
+++ UserRegistrationTest.java
@@ -0,0 +1,14 @@
+package UserRegistration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
+
+public class UserRegistrationTest {
+	private Assertions Assert;
+
+	@Test
+	public void givenFirstNameWhenShouldReturnTrue() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean userTest = userRegistration.firstName();
+		Assert.assertTrue(userTest);
+	}
+}
